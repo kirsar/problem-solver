0306458ff31efb93afa7ea3fcd3753f44c8a8992
ksorokin <ksorokin@netsertive.com>
2022-11-04 00:39:23 +0300

    adds lint and prettier

--- src/lib/problem-solver.spec.ts
+++ src/lib/problem-solver.spec.ts
@@ -1,9 +1,8 @@
-import { ProblemSolver } from "./problem-solver";
+import { ProblemSolver } from './problem-solver';
 
 describe('problem solver tests', () => {
   test('problem solver returns zero', async () => {
     const result = await new ProblemSolver().solve();
     expect(result).toBe(0);
   });
-})
-
+});
